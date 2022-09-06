1aa556c6fda799efa7b05b04f4667c58a73f8485
Dez <warningdez1@gmail.com>
2022-09-06 16:04:33 -0400

    notes and practice

--- Additional_Practice/API/Fox_API_Exercise.py
+++ Additional_Practice/API/Fox_API_Exercise.py
@@ -1,6 +1,6 @@
 
 
 # * PROMPT* 
-# * Using this api: 'http://randomfox.ca/floof', in Python, display a random fox on your browser window.
+# * Using this api: 'http://randomfox.ca/floof', in Python, display a random fox on your browser window. You do not need to register for anything. All that you need from the API is listed here. *
 
 
