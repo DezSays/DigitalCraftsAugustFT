bae34904507457aa9d886ed17d9ad9a36fbc71cd
Dez <warningdez1@gmail.com>
2022-09-06 16:29:02 -0400

    API exercise added to additional practices

---
+++ Additional_Practice/API/python/Fox_API_Hints.py
@@ -0,0 +1,8 @@
+# * HINTS *
+ 
+# import requests and make sure it is installed
+# import webbrowser, no need for installation 
+# set a variable to requests.get('api here')
+# using the variable assigned above, print varaible.json() to see what you are working with
+# access the image
+# use webbrowser.open(final variable) to launch in browser
