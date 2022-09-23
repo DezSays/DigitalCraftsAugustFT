5a4df7e515fb9029daf53ec85472aea5373e23c9
Dez <warningdez1@gmail.com>
2022-09-23 16:00:28 -0400

    SQL practice added

---
+++ Additional_Practice/sql/learningSQL.sql
@@ -0,0 +1,19 @@
+-- Step 1. Create a table and name it students. It should contain the following information: name, website, github_username, point, gender, cohort_start_date, graduated.
+
+-- Step 2. Add three students to your table.
+
+-- Step 3. Update your table so that the student with the lowest points is given 5 more points.
+
+-- Step 4. Update the values in your table where any student who has False for their graduation is changed to True.
+
+-- Step 5. Add two more students to your table.
+
+-- Step 6. Delete one student from your table. 
+
+-- Step 7. Show all students with M selected as their gender. 
+
+-- Step 8. Show which students have graduated. 
+
+-- Step 9. Alter table and add city to your table.
+
+-- Step 10: Get the average points from the table. 
--- week08_BackEnd/node/express/database.js
+++ week08_BackEnd/node/express/database.js
@@ -1,4 +1,10 @@
-// Mock db for right now
-let pokedex = [{"name": 'pikachu', 'hp': 50}, {"name": 'bulbasaur', 'hp': 40}, {"name": 'charmander', 'hp': 50}]
+// // Mock db for right now
+// let pokedex = [{"name": 'pikachu', 'hp': 50}, {"name": 'bulbasaur', 'hp': 40}, {"name": 'charmander', 'hp': 50}]
 
-module.exports = pokedex;
+// module.exports = pokedex;
+
+import postgres from 'postgres'
+
+const sql = postgres({}) // will use psql environment variables
+
+export default sql
