6a5400d8602a33685c9ea72b4afa34f441c7cbf0
Dez <warningdez1@gmail.com>
2022-09-23 16:21:56 -0400

    SQL practice added

--- Additional_Practice/sql/learningSQL.sql
+++ Additional_Practice/sql/learningSQL.sql
@@ -1,19 +1,21 @@
--- Step 1. Create a table and name it students. It should contain the following information: name, website, github_username, points, gender, cohort_start_date, graduated.
+-- Step 1. Create Database Sept2022;
 
--- Step 2. Add three students to your table.
+-- Step 2. Create a table and name it students. It should contain the following information: name, website, github_username, points, gender, cohort_start_date, graduated.
 
--- Step 3. Update your table so that the student with the lowest points is given 5 more points.
+-- Step 3. Add three students to your table.
 
--- Step 4. Update the values in your table where any student who has False for their graduation is changed to True.
+-- Step 4. Update your table so that the student with the lowest points is given 5 more points.
 
--- Step 5. Add two more students to your table.
+-- Step 5. Update the values in your table where any student who has False for their graduation is changed to True.
 
--- Step 6. Delete one student from your table. 
+-- Step 6. Add two more students to your table.
 
--- Step 7. Show all students with M selected as their gender. 
+-- Step 7. Delete one student from your table. 
 
--- Step 8. Show which students have graduated. 
+-- Step 8. Show all students with M selected as their gender. 
 
--- Step 9. Alter table and add city to your table.
+-- Step 9. Show which students have graduated. 
 
--- Step 10: Get the average points from the table. 
+-- Step 10. Alter table and add city to your table.
+
+-- Step 11: Get the average points from the table. 
