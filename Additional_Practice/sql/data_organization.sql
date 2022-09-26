737347dedb0eaabdc4a56d55db1fc85e5cdd5dbc
Dez <warningdez1@gmail.com>
2022-09-26 15:09:00 -0400

    Additional Practices Added

---
+++ Additional_Practice/sql/data_organization.sql
@@ -0,0 +1,31 @@
+-- Step 1. Create a database named mockcompany.
+
+-- Step 2. Create a table named employee, and have the following columns: name, position,currently_employed, gender, years_employed, salary, id.
+
+-- Step 3. Add five employees to your table, 3 that are currently employed and two that are not.
+
+-- Step 4. Display only the names and corresponding id numbers from your table.
+
+-- Step 5. Display only the names and corresponding id numbers of employees that are currently employed.
+
+-- Step 6. Display only the names, corresponding id numbers, and salaries of employees that have been working for more than 5 years.
+
+-- Step 7. Display only the names, corresponding id numbers, and years employed of those who have salaries higher than 75k.
+
+-- Step 8: Display only the names of employees that have a d, upper or lower case, somewhere in their name.
+
+-- Step 9. Display only the names of employees that have an upper case T in their name.
+
+-- Step 10. Take out the gender column from the table.
+
+-- Step 11. Figure out how many records you have in your database.
+
+-- Step 12. Display the name, position, and salary of the highest paid employee.
+
+-- Step 13. Display all employees, sorted in ascending order by name.
+
+-- Step 14. Display all employees, sorted in descending order by salary.
+
+-- Step 15. Display the names and salaries of the top three highest paid employees in decending order.
+
+-- Step 16. Display the name and corresponding id number of the second highest paid employee. 
