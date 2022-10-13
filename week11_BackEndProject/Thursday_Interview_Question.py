760c4994bad14d074592899602c62f527e751901
Dez <warningdez1@gmail.com>
2022-10-13 10:33:20 -0400

    Thursday Interview Question and solution explained

--- week11_BackEndProject/Thursday_Interview_Question.py
+++ week11_BackEndProject/Thursday_Interview_Question.py
@@ -7,9 +7,9 @@ import datetime
 from datetime import datetime
 import calendar
 
-def friday13th(month, year):
-    date = f'{month} 13 {year}'.format()
-    thirteen = datetime.strptime(date, '%m %d %Y').weekday()
+def friday13th(month: int, year: int) -> bool:
+    date = f'{month} 13 {year}'.format()                        # this takes the given info and sets up the date to look like, for example, '5 13 2022' and sets it in a format that we can check below
+    thirteen = datetime.strptime(date, '%m %d %Y').weekday()    # this is using the strptime method to check which day the 13th lands on
     return (calendar.day_name[thirteen]) == "Friday"            # this will return a boolean of either true or false, depending on if the information given contains a friday 13th.
 
 print(friday13th(5,2022))
