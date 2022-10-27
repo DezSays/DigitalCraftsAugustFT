1ccabba6e911279b0dedd302ac9d3c1b9e0dbb58
Dez <warningdez1@gmail.com>
2022-10-27 12:29:09 -0400

    react review

---
+++ week13_Review/review/diff.js
@@ -0,0 +1,43 @@
+
+// Three things that every languages have:
+// 1. memory heap
+// 2. execution context
+// 3. call stack
+
+
+// let count = 0
+
+const counter = () => {
+    let count = 0;
+    count = count+1
+
+    return count
+}
+
+let result = counter() // 1
+result = counter() // 1
+result = counter() // if global, 1 to start
+result = counter() // then 2
+
+class Count {
+    constructor(){
+        this.count = 0;
+    }
+
+    increment(){
+        this.count++
+
+        return this.count
+    }
+}
+
+let count = new Count()
+let res = count.increment() // 1
+let sol = count.constructor() // 0
+
+
+
+
+const greeting = () => {
+    console.log('greetings all')
+}
--- week13_Review/review/src/components/App.js
+++ week13_Review/review/src/components/App.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const App = () => {
+  const [count, setCount] = useState(0)
+  const handleClick = () =>{
+    console.log('button clicked')
+  }
   return (
     <>
-    Howdy Yall
+    <h1>{count}</h1>
+    <button onClick={handleClick}>console log button click</button>
+    <button onClick={()=>setCount(count+1)}>update UI</button>
     </>
   )
 }
 
 export default App
+
+// const wrapper = () => {
+//   return setCount(count+1)
+// }
+// Above is an explicit return, see that the return keyword is used
+
+// Below is an implicit function
+const wrapper = () => setCount(count+1)
