653791399c5fc6aeb7c196f565b8b97bdc2b9489
Dez <warningdez1@gmail.com>
2022-10-27 13:02:39 -0400

    useState notes

--- week13_Review/review/src/components/Forms.js
+++ week13_Review/review/src/components/Forms.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Forms = () => {
+
+  const [count, setCount] = useState(0) // like saying let count = 0
+  const [text, setText] = useState('howdy') // like saying let text = 'howdy'
+  const [isValid, setIsValid] = useState(true) // this sets the checkbox to initially be unchecked
   return (
     <>
-        Forms
+        <h2>{count}</h2>
+        <h2>{text}</h2>
+        <h3>{isValid ? "true" : "false"}</h3>
+
+        <button onClick={() => setCount(count+1)}>click me!</button>
+        <form>
+          {/* how to set text value */}
+          <input type='text' value={text} onChange={e => setText(e.target.value)} />
+          <br />
+
+          {/* how to set a checkbox */}
+          <input type="checkbox" value={isValid} defaultChecked={isValid} onChange={(e) => setIsValid(e.target.checked)}/>
+        </form>
     </>
   )
 }
